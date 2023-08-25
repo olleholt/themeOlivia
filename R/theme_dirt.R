b3c333a44161eb7f2f25c1f459ab0e4abb45e559
olleholt <olivialeighholt97@gmail.com>
2023-08-25 10:59:02 -0700

    Initial commit

---
+++ R/theme_dirt.R
@@ -0,0 +1,6 @@
+theme_dirtboy <- function(){
+  theme(plot.background = element_rect(fill = "tan"),
+        panel.background = element_rect(fill = "lightgreen"),
+        axis.text = element_text(color = "darkgreen"),
+        panel.grid = element_line(color = "brown"))
+}
